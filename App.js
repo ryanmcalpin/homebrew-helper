8ada6a5ad2ae974ac7d85abb000a2b0cb36c79e2
Rygn <rhmcalpin@gmail.com>
2018-05-31 01:06:02 -0700

    add logic for f/c conversion

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, Button, ActivityIndicator, ScrollView, Text, View } from 'react-native';
+import { Alert, Button, ActivityIndicator, ScrollView, Text, TextInput, View } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
 export class HomeScreen extends React.Component {
@@ -26,10 +26,40 @@ export class HomeScreen extends React.Component {
 }
 
 class TempConversionScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {temp: ''};
+  }
+
+  calculate(tempF) {
+    let temp = ((tempF - 32) / 1.8).toFixed(1)
+    this.setState({temp})
+  }
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>F/C Conversion</Text>
+        <TextInput
+          style={{
+            backgroundColor: 'lightgrey',
+            flex: 1,
+            fontSize: 72,
+            textAlign: 'center',
+            width: 200,
+          }}
+          keyboardType='numeric'
+          onChangeText={(temp) => temp == '' ? this.setState({temp}) : this.calculate(temp)}
+        />
+      <Text style={{
+        flex: 1,
+        fontSize: 72,
+        textAlign: 'center',
+        width: 200,
+      }}>
+        {this.state.temp}
+      </Text>
+      <View style={{ flex: 3 }}>
+      </View>
       </View>
     );
   }
@@ -51,7 +81,7 @@ const RootStack = createStackNavigator({
   AnotherOne: AnotherOneScreen,
 },
 {
-  initialRouteName: 'Home',
+  initialRouteName: 'TempConversion',
 });
 
 export default class App extends React.Component {
